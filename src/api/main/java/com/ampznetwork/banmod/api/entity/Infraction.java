e742c01feec53b50e3a801243ac5635206abc13e
Kaley <kaleidox@comroid.org>
2025-08-11 20:28:03 +0200

    punish on pattern

--- src/api/main/java/com/ampznetwork/banmod/api/entity/Infraction.java
+++ src/api/main/java/com/ampznetwork/banmod/api/entity/Infraction.java
@@ -1,5 +1,6 @@
 package com.ampznetwork.banmod.api.entity;
 
+import com.ampznetwork.banmod.api.BanMod;
 import com.ampznetwork.banmod.api.model.PlayerResult;
 import com.ampznetwork.banmod.api.model.Punishment;
 import com.ampznetwork.banmod.api.model.info.DefaultReason;
@@ -11,6 +12,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import lombok.Value;
 import lombok.experimental.FieldDefaults;
 import lombok.experimental.SuperBuilder;
 import org.comroid.api.Polyfill;
@@ -27,7 +29,6 @@ import java.util.Comparator;
 import java.util.Optional;
 import java.util.function.Predicate;
 
-import static lombok.Builder.Default;
 import static java.time.Instant.*;
 import static java.util.function.Predicate.*;
 
@@ -40,55 +41,36 @@ import static java.util.function.Predicate.*;
 @Table(name = "banmod_punishments")
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class Infraction extends DbObject {
-    public static final EntityType<Infraction, Builder<Infraction, ?>> TYPE         = Polyfill.uncheckedCast(new EntityType<>(Infraction::builder,
-            null,
-            Infraction.class,
-            Infraction.Builder.class));
-    public static final Instant                                        TOO_EARLY    = Instant.EPOCH.plus(Duration.ofDays(2));
-    public static final Predicate<Infraction>                          IS_IN_EFFECT = i -> !i.getPunishment().isInherentlyTemporary()
-                                                                                           && (i.getRevoker() == null
-                                                                                               && (i.getExpires() == null || i.getExpires().isAfter(now())
-                                                                                                   || i.getExpires()
-                                                                                                           .isBefore(TOO_EARLY))) /* fix for a conversion bug */;
-    public static       Comparator<Infraction>                         BY_SEVERITY  = Comparator.<Infraction>comparingInt(i ->
-            i.getPunishment().ordinal()).reversed();
-    public static       Comparator<Infraction>                         BY_NEWEST    = Comparator.<Infraction>comparingLong(i ->
-            i.timestamp.toEpochMilli()).reversed();
-    public static       Comparator<Infraction>                         BY_SHORTEST  = Comparator.<Infraction>comparingLong(i ->
-            i.expires == null
-            ? Long.MIN_VALUE
-            : i.expires.toEpochMilli()).reversed();
-    @NotNull
-    @OneToOne
-    Player player;
-    @NotNull
-    @ManyToOne
-    PunishmentCategory category;
-    @NotNull
-    Punishment punishment;
-    @NotNull
-    @lombok.Builder.Default
-    Instant    timestamp = now();
-    @Nullable
-    @lombok.Builder.Default
-    Instant    expires   = null;
-    @Nullable
-    @lombok.Builder.Default
-    String     reason    = null;
-    @ManyToOne Player issuer  = null;
-    @ManyToOne Player revoker = null;
-    @Nullable
-    @lombok.Builder.Default
-    Instant    revokedAt = null;
+    public static final               EntityType<Infraction, Builder<Infraction, ?>> TYPE         = Polyfill.uncheckedCast(
+            new EntityType<>(Infraction::builder,
+                    null,
+                    Infraction.class,
+                    Infraction.Builder.class));
+    public static final               Instant                                        TOO_EARLY    = Instant.EPOCH.plus(
+            Duration.ofDays(2));
+    public static final               Predicate<Infraction>                          IS_IN_EFFECT = i -> !i.getPunishment()
+            .isInherentlyTemporary() && (i.getRevoker() == null && (i.getExpires() == null || i.getExpires()
+            .isAfter(now()) || i.getExpires().isBefore(TOO_EARLY))) /* fix for a conversion bug */;
+    public static                     Comparator<Infraction>                         BY_SEVERITY  = Comparator.<Infraction>comparingInt(
+            i -> i.getPunishment().ordinal()).reversed();
+    public static                     Comparator<Infraction>                         BY_NEWEST    = Comparator.<Infraction>comparingLong(
+            i -> i.timestamp.toEpochMilli()).reversed();
+    public static                     Comparator<Infraction>                         BY_SHORTEST  = Comparator.<Infraction>comparingLong(
+            i -> i.expires == null ? Long.MIN_VALUE : i.expires.toEpochMilli()).reversed();
+    @NotNull @OneToOne                Player                                         player;
+    @NotNull @ManyToOne               PunishmentCategory                             category;
+    @NotNull                          Punishment                                     punishment;
+    @NotNull @lombok.Builder.Default  Instant                                        timestamp    = now();
+    @Nullable @lombok.Builder.Default Instant                                        expires      = null;
+    @Nullable @lombok.Builder.Default String                                         reason       = null;
+    @ManyToOne                        Player                                         issuer       = null;
+    @ManyToOne                        Player                                         revoker      = null;
+    @Nullable @lombok.Builder.Default Instant                                        revokedAt    = null;
 
     public @Nullable String getReason() {
         return Optional.ofNullable(reason)
-                .or(() -> Optional.of(category)
-                        .map(DefaultReason::getDefaultReason)
-                        .filter(not(String::isBlank)))
-                .or(() -> Optional.of(punishment)
-                        .map(DefaultReason::getDefaultReason)
-                        .filter(not(String::isBlank)))
+                .or(() -> Optional.of(category).map(DefaultReason::getDefaultReason).filter(not(String::isBlank)))
+                .or(() -> Optional.of(punishment).map(DefaultReason::getDefaultReason).filter(not(String::isBlank)))
                 .orElseGet(() -> "You were " + punishment.getAdverb());
     }
 
@@ -100,5 +82,32 @@ public class Infraction extends DbObject {
                 timestamp,
                 expires,
                 issuer);
+    }
+
+    public interface Factory {
+        static Factory parse(String expression) {
+        }
+
+        PunishmentCategory getCategory();
+
+        Builder<? extends Infraction, ?> apply(BanMod mod, Builder<? extends Infraction, ?> builder, Player player);
+
+        default Builder<? extends Infraction, ?> create(BanMod mod, Player target) {
+            return apply(mod, builder(), target);
+        }
+    }
+
+    @Value
+    private static class SimpleExpressionFactory implements Factory {
+        PunishmentCategory category;
+        Duration           duration;
+
+        @Override
+        public Builder<? extends Infraction, ?> apply(
+                BanMod mod, Builder<? extends Infraction, ?> builder, Player player) {
+            var rep = mod.findRepetition(player.getId(), category);
+            return builder.category(category).expires(now().plus(duration))
+                    .punishment(category.calculatePunishment(rep).orElse(Punishment.Mute));
+        }
     }
 }
